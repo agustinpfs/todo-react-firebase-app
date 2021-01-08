4d7ada585cb8ce0e80f6cc2d208a40c8430e4b8e
Agustin <agu@cursosit.com>
2021-01-08 13:14:58 -0300

    add delete icon (material-ui)

--- src/Todo.js
+++ src/Todo.js
@@ -2,17 +2,15 @@ import { Button, Avatar, List, ListItem, ListItemAvatar, ListItemText, ImageIcon
 import React from 'react';
 import './Todo.css';
 import db from './firebase';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 
 function Todo(props) {
     return (
         <List>
             <ListItem className='todo__list'>
-                <ListItemAvatar>
-
-                </ListItemAvatar>
                 <ListItemText primary={props.todo.todo} secondary='todo...' />
             </ListItem>
-            <Button onClick={event => db.collection('todos').doc(props.todo.id).delete()}>DELETE</Button>
+            <DeleteForeverIcon onClick={event => db.collection('todos').doc(props.todo.id).delete()}>DELETE</DeleteForeverIcon>
         </List>
     )
 }
